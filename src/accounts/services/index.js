72126bcf79e9b4ca1a852a7e0365623ea93d9e5b
Hanuma Satya Sunnam <20096284@mail.wit.ie>
2022-05-17 20:10:48 +0100

    JSON Tokens & Register/Authenicate Users

--- src/accounts/services/index.js
+++ src/accounts/services/index.js
@@ -2,17 +2,18 @@ import Account from '../entities/Account';
 
   export default {
 
-    authenticate: async (email, password, {accountsRepository, authenticator}) => {
+    authenticate: async (email, password, {accountsRepository, authenticator, tokenManager}) => {
       const account = await accountsRepository.getByEmail(email);
       const result = await authenticator.compare(password, account.password);
       if (!result) {
           throw new Error('Bad credentials');
       }
-      const token = JSON.stringify({ email: account.email });//JUST Temporary!!! TODO: make it better
+      const token = tokenManager.generate({ email: account.email });//JUST Temporary!!! TODO: make it better
       return token;
     },
     
-    registerAccount: async  (firstName, lastName, email, password, {accountsRepository}) => {
+    registerAccount: async  (firstName, lastName, email, password, {accountsRepository, authenticator}) => {
+      password = await authenticator.encrypt(password);
       const account = new Account(undefined, firstName, lastName, email, password);
       return accountsRepository.persist(account);
     },
